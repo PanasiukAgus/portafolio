b909445e6589f7fcc1d1a05a86aaec18dd004c5f
Agustina Panasiuk <116914199+PanasiukAgus@users.noreply.github.com>
2026-08-01 02:12:05 -0300

    Remove Google Tag Manager scripts
    Removed Google Tag Manager scripts from __root.tsx.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,20 +28,7 @@ function NotFoundComponent() {
           >
             Go home
           </Link>
-          scripts: [
-      {
-        src: "https://www.googletagmanager.com/gtag/js?id=G-VX3Q57FM5L",
-        async: true,
-      },
-      {
-        children: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-VX3Q57FM5L');
-        `,
-      },
-    ],
+         
         </div>
       </div>
     </div>
